29f5cae334eed8c07a91dde75294a7c474968f05
0-src <119033688+0-src@users.noreply.github.com>
2025-05-04 00:25:17 -0500

    Add project files.

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace TradeExec.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private bool _isLoginMode = true;
+
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set { _username = value; OnPropertyChanged(); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        public string FormTitle => _isLoginMode ? "Login" : "Create new account.";
+        public string FormSubtitle => _isLoginMode ? "START TRADING" : "START A NEW ACCOUNT";
+        public string ActionButtonText => _isLoginMode ? "Login" : "Create Account";
+
+        public ICommand SubmitCommand { get; }
+        public ICommand ToggleModeCommand { get; }
+
+
+        public LoginViewModel()
+        {
+            SubmitCommand = new RelayCommand(Submit);
+            ToggleModeCommand = new RelayCommand(ToggleMode);
+        }
+
+        private void Submit()
+        {
+            if (_isLoginMode)
+            {
+                System.Diagnostics.Debug.WriteLine($"[LOGIN] {Username} / {Password}");
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine($"[CREATE] {Username} / {Password}");
+            }
+        }
+
+        public Action OnModeSwitched;
+        private void ToggleMode()
+        {
+            _isLoginMode = !_isLoginMode;
+            OnPropertyChanged(nameof(FormTitle));
+            OnPropertyChanged(nameof(FormSubtitle));
+            OnPropertyChanged(nameof(ActionButtonText));
+
+            OnModeSwitched?.Invoke();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Views/LoginView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using TradeExec.ViewModels;
+
+namespace TradeExec.Views
+{
+    public partial class LoginView : UserControl
+    {
+        private LoginViewModel _vm;
+        public LoginView()
+        {
+            InitializeComponent();
+            _vm = new LoginViewModel();
+            this.DataContext = _vm;
+
+            _vm.OnModeSwitched += AnimateFormModeSwitch;
+        }
+        private void AnimateFormModeSwitch()
+        {
+            var sb = (Storyboard)this.Resources["FadeTextStoryboard"];
+            sb.Begin();
+        }
+    }
+}
